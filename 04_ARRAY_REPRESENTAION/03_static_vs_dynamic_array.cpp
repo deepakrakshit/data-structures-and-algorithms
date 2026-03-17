a26afa4b6577d393ef0672d9a8f4baacfec5d6b8
Deepak Rakshit <deepakrakshit505@gmail.com>
2026-03-17 14:40:40 +0530

    Add files via upload

---
+++ 04_ARRAY_REPRESENTAION/03_static_vs_dynamic_array.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+/*
+    STATIC vs DYNAMIC ARRAY
+
+    Static Array:
+    - Size is fixed at compile time
+    - Stored in stack memory
+
+    Dynamic Array:
+    - Size is decided at runtime
+    - Stored in heap memory
+    - Created using new operator
+*/
+
+int main()
+{
+    // Static array
+    int A[5] = {1, 2, 3, 4, 5};
+
+    cout << "Static Array:" << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << A[i] << " ";
+    }
+
+    cout << endl;
+
+    // Dynamic array
+    int *p = new int[5];  // allocated in heap
+
+    // (C style) dynamic allocation:
+    // int *p = (int *)malloc(5 * sizeof(int));
+
+    // assigning values
+    for (int i = 0; i < 5; i++)
+    {
+        p[i] = (i + 1) * 10;
+    }
+
+    cout << "\nDynamic Array:" << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << p[i] << " ";
+    }
+
+    // free memory
+    delete[] p;
+
+    // (C style) free memory:
+    // free(p);
+
+    return 0;
+}
