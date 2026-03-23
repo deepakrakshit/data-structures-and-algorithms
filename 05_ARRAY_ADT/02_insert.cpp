81f1213c695953004cd2fdfe94c76146d150fd54
Deepak Rakshit <deepakrakshit505@gmail.com>
2026-03-23 22:38:37 +0530

    Refactor array insertion code for clarity and safety

--- 05_ARRAY_ADT/02_insert.cpp
+++ 05_ARRAY_ADT/02_insert.cpp
@@ -2,39 +2,46 @@
 using namespace std;
 
 int main() {
-
     int length, size;
-    cout << "Enter Length and size of the array: " << endl;
+
+    // length = total capacity, size = current elements
+    cout << "Enter capacity and current size: ";
     cin >> length >> size;
 
-    int arr[length];
+    int arr[100]; // fixed size to avoid VLA issues
 
-    cout << "Enter the elements: " << endl;
+    // input elements
+    cout << "Enter elements:\n";
     for (int i = 0; i < size; i++) {
         cin >> arr[i];
     }
 
-    if(size >= length){
-        cout << "Array is full, cannot insert!";
+    // check if array is full
+    if (size == length) {
+        cout << "Array is full!";
         return 0;
     }
 
     int index, value;
-    cout << "Enter Index and value of the element you wish to insert: " << endl;
+    cout << "Enter index and value to insert: ";
     cin >> index >> value;
 
-    if(index >= 0 && index <= size){
-        for (int i = size; i > index; i--) {
-            arr[i] = arr[i - 1];
-        }
-
-        arr[index] = value;
-        size++;
-    } else {
+    // check valid index
+    if (index < 0 || index > size) {
         cout << "Invalid index!";
         return 0;
     }
 
+    // shift elements to right
+    for (int i = size; i > index; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    // insert value
+    arr[index] = value;
+    size++;
+
+    // display updated array
     cout << "Updated array: ";
     for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
